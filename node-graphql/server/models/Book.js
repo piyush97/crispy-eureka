ea6b04799886c3811199103547cd84b78c247242
Piyush Mehta <me@piyushmehta.com>
2019-02-27 23:15:01 +0530

    structure

---
+++ node-graphql/server/models/Book.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var graphqlHTTP = require('express-graphql');
+var schema = require('./graphql/bookSchema');
+var cors = require("cors");
+
+var BookSchema = new mongoose.Schema({
+  id: String,
+  isbn: String,
+  title: String,
+  author: String,
+  description: String,
+  published_year: { type: Number, min: 1945, max: 2019 },
+  publisher: String,
+  updated_date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Book', BookSchema);
